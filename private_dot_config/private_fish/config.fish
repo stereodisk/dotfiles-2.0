aff76b7677ffe856a306b5c52f7cb7a0f9260697
stereodisk <jesichiko@gmail.com>
2026-01-12 23:32:54 -0600

    config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -24,3 +24,14 @@ starship init fish | source
 set -U fish_autosuggestion_strategy history
 set -U fzf_complete 1
 set -U fzf_preview_dir_cmd 'ls -la'
+
+# WAYLAND & SWAY VARIABLES
+set -Ux XDG_CURRENT_DESKTOP sway
+set -Ux XDG_SESSION_DESKTOP sway
+set -Ux XDG_SESSION_TYPE wayland
+set -Ux GDK_BACKEND wayland,x11
+set -Ux QT_QPA_PLATFORM wayland;xcb
+set -Ux SDL_VIDEODRIVER wayland
+set -Ux CLUTTER_BACKEND wayland
+set -Ux MOZ_ENABLE_WAYLAND 1
+set -Ux _JAVA_AWT_WM_NONREPARENTING 1
